e5932df6802d9c7ce9dd14c80ea4078aeb3e9efd
DESKTOP-JGH975K\YJ <d>
2022-04-25 13:53:46 +0900

    첫 구동?

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+var express = require('express')
+var app = express() // app : 객체
+
+//사용자가 localhost:5000/hello URL 주소로 접근했을 때
+app.get('hello',(req, res)=>{
+    res.send('hello world!') // 응답을 브라우저로 보내줌
+})
+
+//맨 밑에 놓기
+app.listen(5000, () => {
+    console.log('server is running port 5000 !')
+})
